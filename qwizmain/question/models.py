d037be4cf1fd7fce1ed2c78bbec2d46c3620fd41
Cristian <cristis991@mail.ru>
2023-03-03 23:34:10 +0200

    commit 2

--- qwizmain/question/models.py
+++ qwizmain/question/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+class Question (models.Model):
+    choices_var = [('1', '1',),
+                   ('2', '2'),
+                   ('3', '3'),
+                   ('4', '4')
+
+                   ]
+
+    question = models.CharField(max_length=500)
+    a1=models.CharField(max_length=300,verbose_name='ответ_1')
+    a2 = models.CharField(max_length=300,verbose_name='ответ_2')
+    a3 = models.CharField(max_length=300,verbose_name='ответ_3')
+    a4 = models.CharField(max_length=300,verbose_name='ответ_4')
+    correct = models.CharField(choices=choices_var,max_length=10)
+
+
+
+
